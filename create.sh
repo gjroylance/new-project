6142d1b0e8d18b0f232c3aae592f2f7f67483fec
Gordon Roylance <gordon.roylance@beanstalkhq.com>
2015-04-16 15:22:00 -0600

    Improving script to use containing directory as company name.

--- create.sh
+++ create.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+dir=$(pwd | sed 's/\/[^/]*$//' | sed 's/.*\///' )
+
 mkdir -p ../$1
 cp .gitignore ../$1/
 sed "s/new-project/$1/g" build.gradle > ../$1/build.gradle
+sed -in "s/company-name/$dir/g" ../$1/build.gradle
 sed "s/new-project/$1/g" settings.gradle > ../$1/settings.gradle
 mkdir -p ../$1/src/main/java ../$1/src/main/resources
 mkdir -p ../$1/src/test/java ../$1/src/test/resources
+
