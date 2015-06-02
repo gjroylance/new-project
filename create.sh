67ae8fb46840d530e66582a35b36ecb3bde04ba4
Gordon Roylance <gordon.roylance@beanstalkhq.com>
2015-06-02 08:47:36 -0600

    Adding vertx shared dependencies

--- create.sh
+++ create.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 dir=$(pwd | sed 's/\/[^/]*$//' | sed 's/.*\///' )
+name=$(echo $1 | sed 's/-service//g' | sed 's/-//g' | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2)) }')
 
 mkdir -p ../$1
 cp .gitignore ../$1/
@@ -10,9 +11,20 @@ sed "s/new-project/$1/g" settings.gradle > ../$1/settings.gradle
 mkdir -p ../$1/src/main/java ../$1/src/main/resources
 mkdir -p ../$1/src/test/java ../$1/src/test/resources
 
-cp ../vertx-shared/vertx.gradle .
-cp ../vertx-shared/gradle.properties .
+cp ../vertx-shared/vertx.gradle ../$1/vertx.gradle
+sed "s/vertx-shared/$1/g" gradle.properties > ../$1/gradle.properties
 
+mkdir -p ../$1/src/main/java/com/canopy/service
+sed "s/XService/${name}Service/g" XService.java > ../$1/src/main/java/com/canopy/service/${name}Service.java
+
+lf=$'\n'
+#cp ../all/build.gradle ../all/build.gradle.bak
+sed -i '' "s/\(^.*if.*$\)/\1\\$lf        compile project(':$1')/" ../all/build.gradle
+sed -i '' "s/\(^.*else.*$\)/\1\\$lf        compile 'com.canopy.$1:$1:+'/" ../all/build.gradle
+
+cd ../$1
 git init
-git commit -a -m "Initial $1 setup"
+git add .
+git commit -m "Initial $1 setup"
 git remote add origin https://github.com/CanopyTax/$1.git
+cd -
